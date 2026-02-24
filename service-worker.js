fe8b5bfacb8c405a7a5d0269bbfd52a42e8beb6f
yakheyeon <123567852+yakheyeon@users.noreply.github.com>
2026-02-25 00:44:24 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "pwa-cache-v1";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
